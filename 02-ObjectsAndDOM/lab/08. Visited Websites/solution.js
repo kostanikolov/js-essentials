f8d4858c57fdef8acd8cffe019f2536705130fbb
kostanikolov <kosta94@mail.bg>
2019-05-22 13:53:15 +0300

    changes

--- 02-ObjectsAndDOM/lab/08. Visited Websites/solution.js
+++ 02-ObjectsAndDOM/lab/08. Visited Websites/solution.js
@@ -1,4 +1,21 @@
 function solve() {
 
-  //TODO...
+    function clickEvent() {
+      let textElement = this.parentNode.children[1];
+      console.log(textElement);
+      let text = textElement.innerText.toString();
+      let number = text.match(/\d+/)[0];
+      text = text.replace(number, (+number + 1).toString());
+      textElement.innerHTML = text;
+    }
+
+    //add event to links
+    let linkElements = document.getElementsByClassName('link-1');
+
+    //console.log(linkElements);
+
+    for (let linkElement of linkElements) {
+      //console.log(linkElement);
+      linkElement.addEventListener('click', clickEvent);
+    }
 }
