07326bc6eae189947ca7bb2d5daf632112a11401
kostanikolov <kosta94@mail.bg>
2019-05-26 15:56:29 +0300

    modified files

--- 02-ObjectsAndDOM/exercise/01. Cards/solution.js
+++ 02-ObjectsAndDOM/exercise/01. Cards/solution.js
@@ -1,35 +1,95 @@
 function solve() {
    
-   let playerOne = document.getElementById("player1Div");
-   let playerTwo = document.getElementById("player2Div");
-   
-   let result = document.querySelector("#result");
-   
-   let leftSpan = result.children[0];
-   let rightSpan = result.children[2];
+    let playerOneDiv = document.getElementById("player1Div").children;
+    let playerTwoDiv = document.getElementById("player2Div").children;
+    
+    let resultDiv = document.getElementById("result").children;
+    let leftCardChoice = resultDiv[0];
+    let rightCardChoice = resultDiv[2];
 
-   let leftValue = 0;
-   let rightValue = 0;
-   
-   for (let card of playerOne.children) {
-      card.addEventListener("click", () => {
-         let cardValue = card.getAttribute("name");
-         leftSpan.innerHTML = cardValue;
-         result.children[0] = leftSpan;
-         leftValue = Number(leftSpan.innerHTML);
-         console.log(leftValue);
-      })
-   }
+    let leftCardImg = "";
+    let rightCardImg = "";
 
-   for (let card of playerTwo.children) {
-      card.addEventListener("click", () => {
-         let cardValue = card.getAttribute("name");
-         rightSpan.innerHTML = cardValue;
-         result.children[2] = rightSpan;
-         rightValue = Number(rightSpan.innerHTML);
-         console.log(rightValue);
-      })
-   }
-   
-   console.log(leftValue + rightValue);
+    let historyDiv = document.getElementById("history");
+    
+    // for (let card of playerOneDiv.children) {
+    for (let card = 0; card < playerOneDiv.length; card++) {
+        let cardImgElement = playerOneDiv[card];
+
+        cardImgElement.addEventListener("click", () => {
+            leftCardImg = cardImgElement;
+
+            let cardName = cardImgElement.getAttribute("name");
+            leftCardChoice.innerHTML = cardName;
+
+            cardImgElement.setAttribute("src", "images/whiteCard.jpg");
+
+            if (rightCardChoice.innerHTML !== "") {
+                let leftCardValue = Number(leftCardChoice.innerHTML);
+                let rightCardValue = Number(rightCardChoice.innerHTML);
+
+                if (leftCardValue > rightCardValue) {
+                    leftCardImg.setAttribute("style", "border: 2px solid green");
+                    rightCardImg.setAttribute("style", "border: 2px solid red");
+
+                    // let newSpanElement = document.createElement("span");
+                    // newSpanElement.innerHTML = `[${leftCardValue} vs ${rightCardValue}] `;
+                    historyDiv.innerHTML += `[${leftCardValue} vs ${rightCardValue}] `;
+
+                    leftCardChoice.innerHTML = "";
+                    rightCardChoice.innerHTML = "";
+                } else if (rightCardValue > leftCardValue) {
+                    rightCardImg.setAttribute("style", "border: 2px solid green");
+                    leftCardImg.setAttribute("style", "border: 2px solid red");
+
+                    // let newSpanElement = document.createElement("span");
+                    // newSpanElement.innerHTML = `[${leftCardValue} vs ${rightCardValue}] `;
+                    historyDiv.innerHTML += `[${leftCardValue} vs ${rightCardValue}] `;
+
+                    leftCardChoice.innerHTML = "";
+                    rightCardChoice.innerHTML = "";
+                }
+            }
+        });
+    }
+
+    // for (let card of playerTwoDiv.children) {
+    for (let card = 0; card < playerTwoDiv.length; card++) {
+        let cardImgElement = playerTwoDiv[card];
+
+        cardImgElement.addEventListener("click", () => {
+            rightCardImg = cardImgElement;
+
+            let cardName = cardImgElement.getAttribute("name");
+            rightCardChoice.innerHTML = cardName;
+
+            cardImgElement.setAttribute("src", "images/whiteCard.jpg");
+            
+            if (leftCardChoice.innerHTML !== "") {
+                let leftCardValue = Number(leftCardChoice.innerHTML);
+                let rightCardValue = Number(rightCardChoice.innerHTML);
+
+                if (leftCardValue > rightCardValue) {
+                    leftCardImg.setAttribute("style", "border: 2px solid green");
+                    rightCardImg.setAttribute("style", "border: 2px solid red");
+
+                    // let newSpanElement = document.createElement("span");
+                    // newSpanElement.innerHTML = `[${leftCardValue} vs ${rightCardValue}] `;
+                    historyDiv.innerHTML += `[${leftCardValue} vs ${rightCardValue}] `;
+
+                    leftCardChoice.innerHTML = "";
+                    rightCardChoice.innerHTML = "";
+                } else if (rightCardValue > leftCardValue) {
+                    rightCardImg.setAttribute("style", "border: 2px solid green");
+                    leftCardImg.setAttribute("style", "border: 2px solid red");
+
+                    // let newSpanElement = document.createElement("span");
+                    // newSpanElement.innerHTML = `[${leftCardValue} vs ${rightCardValue}] `;
+                    historyDiv.innerHTML += `[${leftCardValue} vs ${rightCardValue}] `;
+                    leftCardChoice.innerHTML = "";
+                    rightCardChoice.innerHTML = "";
+                }
+            }
+        });
+    }
 }
