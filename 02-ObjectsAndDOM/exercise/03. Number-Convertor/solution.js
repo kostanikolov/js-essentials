99d73bdc21fb677fd98f1c63111619d1169db89d
kostanikolov <kosta94@mail.bg>
2019-05-27 10:18:46 +0300

    modified files

--- 02-ObjectsAndDOM/exercise/03. Number-Convertor/solution.js
+++ 02-ObjectsAndDOM/exercise/03. Number-Convertor/solution.js
@@ -1,4 +1,30 @@
 function solve() {
 
-    //TODO...
+    let containerDivElement = document.getElementById("container").children
+    let convertItButton = containerDivElement[containerDivElement.length - 1];
+
+    let selectMenuToElement = document.getElementById("selectMenuTo");
+    let binaryOptionElement = selectMenuToElement.children[0];
+    binaryOptionElement.setAttribute("value", "binary");
+    binaryOptionElement.innerHTML = "Binary";
+
+    let newHexadecimalOptionElement = document.createElement("option");
+    newHexadecimalOptionElement.setAttribute("value", "hexadecimal");
+    newHexadecimalOptionElement.innerHTML = "Hexadecimal";
+
+    selectMenuToElement.appendChild(newHexadecimalOptionElement);
+    
+    convertItButton.addEventListener("click", () => {
+        let decimalNumber = Number(document.getElementById("input").value);
+        let selectedOption = selectMenuToElement.options[selectMenuToElement.selectedIndex].text;
+        let result = document.getElementById("result");
+
+        if (selectedOption === "Binary") {
+            let binaryNumber = decimalNumber.toString(2);
+            result.setAttribute("value", binaryNumber);
+        } else if (selectedOption === "Hexadecimal") {
+            let hexNumber = decimalNumber.toString(16);
+            result.setAttribute("value", hexNumber.toUpperCase());
+        }
+    });
 }
