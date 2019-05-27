89efd215e61197ebc3bd870d70b4dcf1dedba4a9
kostanikolov <kosta94@mail.bg>
2019-05-27 18:08:31 +0300

    modified files

--- 03-ArraysAndMatrices/lab/lastKNumsSeq.js
+++ 03-ArraysAndMatrices/lab/lastKNumsSeq.js
@@ -1,19 +1,37 @@
 function solve(n, k) {
-    
-    let n1 = 1;
-    let arr = [ n1 ];
-    let sum = 0;
-    
-    for (let index = 0; index < n; index++) {
-        let slicedArr = arr.slice(index - k, index);
-        sum = slicedArr.reduce((acc, val) => acc + val);
-        arr.push(sum);
-        sum = 0;
-    }
-    console.log(arr);
 
-    // 1 1 2 4 7 13
-    // 1 1 2 3 5 8 13 21
+    let arr = [1];
+
+    for (let current = 1; current < n; current++) {
+        
+        let start = Math.max(0, current - k);
+        let end = current - 1;
+        let sum = 0;
+
+        for (let prev = start; prev <= end; prev++) {
+            sum += arr[prev];
+        }
+
+        arr[current] = sum;
+    }
+
+    console.log(arr.join(' '));
+
+    // let nums = [ 1 ];
+
+    // for (let current = 1; current < n; current++) {
+    //     let sum = 0;
+
+    //     for (let prev = current - k; prev <= current - 1; prev++) {
+    //        if (prev >= 0) {
+    //            sum += nums[prev];
+    //        }
+    //     }
+
+    //     nums[current] = sum;
+    // }
+
+    // console.log(nums.join(' '));
 }
 
 solve(6, 3);
