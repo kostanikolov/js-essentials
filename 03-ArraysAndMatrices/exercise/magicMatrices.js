783bdbfaa9783f26209f1ea6e7739a7f36c05f8d
kostanikolov <kosta94@mail.bg>
2019-05-30 19:28:46 +0300

    modified

--- 03-ArraysAndMatrices/exercise/magicMatrices.js
+++ 03-ArraysAndMatrices/exercise/magicMatrices.js
@@ -1,32 +1,46 @@
-function solve(params) {
-    let rowSum = 0;
-    let colSum = 0;
+function solve(matrix) {
 
-    for (let row = 0; row < params.length; row++) {
-       for (let col = 0; col < params[row].length; col++) {
-           rowSum += params[row][col];
-           colSum += params[col][row];
-       }
+    let sum = 0;
+    matrix[0].forEach(x => sum += x);
+
+    for (let row = 1; row < matrix.length; row++) {
+        let rowSum = 0;
+        matrix[row].forEach(x => rowSum += x);
+
+        if (rowSum !== sum) {
+            return false;
+        }
     }
 
-    console.log(rowSum);
-    console.log(colSum);
+    for (let col = 0; col < matrix[0].length; col++) {
+        let colSum = 0;
+
+        for (let row = 0; row < matrix.length; row++) {
+            colSum += matrix[row][col];
+        }
+
+        if (colSum !== sum) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-solve([
+console.log(solve([
     [4, 5, 6],
     [6, 5, 4],
     [5, 5, 5]
-]);
+]));
 
-solve([
+console.log(solve([
     [11, 32, 45],
     [21, 0, 1],
     [21, 1, 1]
-]);
+]));
 
-solve([
+console.log(solve([
     [1, 0, 0],
     [0, 0, 1],
-    [0, 1, 0]   
-]);
+    [0, 1, 0]
+]));
