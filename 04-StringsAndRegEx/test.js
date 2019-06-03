3584d4dec51ea75b98d20eb52a3ee2500cc40933
kostanikolov <kosta94@mail.bg>
2019-06-03 13:15:12 +0300

    modified files

--- 04-StringsAndRegEx/test.js
+++ 04-StringsAndRegEx/test.js
@@ -1,5 +1,13 @@
 function main(args) {
     
+    let arr = [10,20,30];
+
+    if (true) {
+        let arr = ['gosho', 'pesho'];
+        console.log(arr); // ['gosho', 'pesho']
+    }
+
+    console.log(arr); // [10, 20, 30]
 }
 
 main();
